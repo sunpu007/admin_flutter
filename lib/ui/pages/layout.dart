213b4231256578bb9a114348b1a5157e2136d740
Jerry <731238433@qq.com>
2022-05-05 21:54:35 +0800

    layout

---
+++ lib/ui/pages/layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Layout extends StatelessWidget {
+  const Layout({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Layout'),
+    );
+  }
+}
