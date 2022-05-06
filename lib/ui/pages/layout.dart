257c31b130bb68d42be28df6281b9738597fa8b4
Jerry <731238433@qq.com>
2022-05-06 21:48:35 +0800

    layout布局

--- lib/ui/pages/layout.dart
+++ lib/ui/pages/layout.dart
@@ -5,8 +5,57 @@ class Layout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Layout'),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 210,
+            height: double.infinity,
+            color: const Color(0XFF304156),
+          ),
+          Expanded(
+            child: Column(
+              children: const [
+                HamburgerWidget()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HamburgerWidget extends StatelessWidget {
+  const HamburgerWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0X22000000),
+            // spreadRadius: .5,
+            blurRadius: 4,
+          )
+        ]
+      ),
+      child: Row(
+        children: const [
+          Icon(Icons.menu_open),
+          // IconButton(
+          //   icon: Icon(Icons.menu_open),
+          //   onPressed: () {},
+          // ),
+        ],
+      ),
     );
   }
 }
